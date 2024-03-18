a8d7d5bf8853f7720b0702462711914c3122d4a7
Beatsfitness <157222620+Beatsfitness@users.noreply.github.com>
2024-03-19 00:40:05 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    baseURL: "http://ec2-35-154-153-93.ap-south-1.compute.amazonaws.com:8080/api/v1/", // replace with your actual base URL
+    baseURL: "http://ec2-35-154-153-93.ap-south-1.compute.amazonaws.com:8080", // replace with your actual base URL
     apiEndpoint: "/api/v1",
   };
   
